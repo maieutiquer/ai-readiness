21f10d990c1f37435a19cc8330508a4dbe2df77d
Daniel Rodriguez <maieutiquer@gmail.com>
2025-02-25 00:10:17 +0200

    refactor: combine types and constants into formDefinitions

--- src/app/_components/form/components/SelectField.tsx
+++ src/app/_components/form/components/SelectField.tsx
@@ -15,7 +15,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "~/components/ui/select";
-import type { FormValues } from "../types";
+import type { FormValues } from "../formDefinitions";
 
 interface SelectFieldProps {
   control: Control<FormValues>;
