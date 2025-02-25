28488bf92c2811c3c0cbcb163e3c41e4d43b1606
Daniel Rodriguez <maieutiquer@gmail.com>
2025-02-25 02:56:42 +0200

    build: use pnpm cache as per docs

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // the project has ESLint errors.
-    ignoreDuringBuilds: process.env.CI === "true",
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: process.env.CI === "true",
-  },
-};
+const config = {};
 
 export default config;
