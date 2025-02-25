e4ee97f2db9a1895f5fd0a38ebe64f0a06e5b092
Daniel Rodriguez <maieutiquer@gmail.com>
2025-02-25 04:44:58 +0200

    chore: update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+// @ts-check
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default tseslint.config(
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // "@typescript-eslint/array-type": "off",
+      // "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+    },
+  },
+);
