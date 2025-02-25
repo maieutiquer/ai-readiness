d331f5901f554853f782c0a4334aca25687fe883
Daniel Rodriguez <maieutiquer@gmail.com>
2025-02-25 02:49:51 +0200

    build: skip checking types in ci

--- next.config.js
+++ next.config.js
@@ -9,7 +9,14 @@ const config = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // the project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: process.env.CI === "true",
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: process.env.CI === "true",
   },
 };
 
